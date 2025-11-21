be1617af4d1f125fd5eec2e3f2d85e0581998c92
werb210 <todd.w@boreal.financial>
2025-11-21 08:30:29 -0700

    Refactor routing with protected layout shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { router } from "@/router/index";
-import AuthProvider from "@/context/AuthContext";
-import "@/index.css";
-
-const qc = new QueryClient();
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={qc}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
