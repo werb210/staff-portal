18ee6bcc53e37fcaeb4ce929821eb95aa44841ff
werb210 <todd.w@boreal.financial>
2025-12-03 13:02:43 -0700

    Simplify routes to health endpoint

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,5 @@
 import { Router } from "express";
 
-import contactsController from "../controllers/contactsController";
-import companiesController from "../controllers/companiesController";
-import productsController from "../controllers/productsController";
-import tagController from "../controllers/tagController";
-
 const router = Router();
 
 // Internal health check
@@ -15,32 +10,5 @@ router.get("/_int/health", (_req, res) => {
     timestamp: new Date().toISOString(),
   });
 });
-
-// Contacts CRUD
-router.get("/contacts", contactsController.list);
-router.get("/contacts/:id", contactsController.get);
-router.post("/contacts", contactsController.create);
-router.put("/contacts/:id", contactsController.update);
-router.delete("/contacts/:id", contactsController.remove);
-
-// Companies CRUD
-router.get("/companies", companiesController.list);
-router.get("/companies/:id", companiesController.get);
-router.post("/companies", companiesController.create);
-router.put("/companies/:id", companiesController.update);
-router.delete("/companies/:id", companiesController.remove);
-
-// Products CRUD
-router.get("/products", productsController.list);
-router.get("/products/:id", productsController.get);
-router.post("/products", productsController.create);
-router.put("/products/:id", productsController.update);
-router.delete("/products/:id", productsController.remove);
-
-// Tags CRUD
-router.get("/tags", tagController.list);
-router.post("/tags", tagController.create);
-router.put("/tags/:id", tagController.update);
-router.delete("/tags/:id", tagController.remove);
 
 export default router;
