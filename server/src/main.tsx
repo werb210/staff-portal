ea7780f564eaaa026843d581021209a6af75cf46
werb210 <todd.w@boreal.financial>
2025-12-04 11:37:09 -0700

    Update main.tsx

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { appRouter } from "@/router/appRouter";
-import { useAuthStore } from "@/state/authStore";
-
-useAuthStore.getState().init();
+import "@/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
