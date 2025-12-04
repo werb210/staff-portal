adf34121e30924465c0bb6298dd345abf4de9127
werb210 <todd.w@boreal.financial>
2025-12-04 11:24:34 -0700

    Update Unauthorized.tsx

--- server/src/pages/Unauthorized.tsx
+++ server/src/pages/Unauthorized.tsx
@@ -1,6 +1,6 @@
 export default function Unauthorized() {
   return (
-    <div style={{ padding: "40px" }}>
+    <div style={{ padding: 40 }}>
       <h1>Unauthorized</h1>
       <p>You do not have permission to view this page.</p>
     </div>
