2763249e3f2bdc9e7df3145da6106d7ab081a96a
werb210 <todd.w@boreal.financial>
2025-12-04 11:42:54 -0700

    Update Referrals.tsx

--- server/src/pages/Referrals.tsx
+++ server/src/pages/Referrals.tsx
@@ -1,8 +1,3 @@
 export default function Referrals() {
-  return (
-    <div>
-      <h1>Referrals</h1>
-      <p>Referrer dashboard placeholder.</p>
-    </div>
-  );
+  return <h1>Referrals</h1>;
 }
