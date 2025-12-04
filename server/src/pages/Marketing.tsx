ef79dd3751074bd501b4a26d70fb2cdf0770f7b9
werb210 <todd.w@boreal.financial>
2025-12-04 11:42:39 -0700

    Update Marketing.tsx

--- server/src/pages/Marketing.tsx
+++ server/src/pages/Marketing.tsx
@@ -1,8 +1,3 @@
 export default function Marketing() {
-  return (
-    <div>
-      <h1>Marketing</h1>
-      <p>Campaigns, analytics, sequences.</p>
-    </div>
-  );
+  return <h1>Marketing</h1>;
 }
