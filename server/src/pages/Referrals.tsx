e25c76843039ad1507410b606825bda1dd875f4c
werb210 <todd.w@boreal.financial>
2025-12-04 10:55:15 -0700

    Update Referrals.tsx

--- server/src/pages/Referrals.tsx
+++ server/src/pages/Referrals.tsx
@@ -1,14 +1,8 @@
-import MainLayout from "../layouts/MainLayout";
-
 export default function Referrals() {
   return (
-    <MainLayout>
-      <div className="bf-page">
-        <h1 className="bf-page-title">Referrals</h1>
-        <p className="bf-page-description">
-          Track referral partners, submissions, and commissions.
-        </p>
-      </div>
-    </MainLayout>
+    <div>
+      <h1>Referrals</h1>
+      <p>Referrer dashboard placeholder.</p>
+    </div>
   );
 }
