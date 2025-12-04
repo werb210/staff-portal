ab1c19f02513059d3fb6d8eb52b359812c1a47b2
werb210 <todd.w@boreal.financial>
2025-12-04 11:46:50 -0700

    Update Unauthorized.tsx

--- server/src/pages/Unauthorized.tsx
+++ server/src/pages/Unauthorized.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Unauthorized() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Unauthorized</h1>
-      <p>You do not have permission to view this page.</p>
+      <p>You do not have permission to access this page.</p>
+      <p>
+        <Link to="/">Go back to dashboard</Link>
+      </p>
     </div>
   );
 }
