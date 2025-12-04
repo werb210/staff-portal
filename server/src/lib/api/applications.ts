afe7362a9c9d4aae9115ca2824dcf271033d6815
werb210 <todd.w@boreal.financial>
2025-12-04 11:28:34 -0700

    Update applications.ts

--- server/src/lib/api/applications.ts
+++ server/src/lib/api/applications.ts
@@ -1,36 +1,7 @@
-import api from "@/lib/api/http";
+import { api } from "@/lib/apiClient";
+import { httpGet } from "@/lib/http";
+import { getToken } from "@/utils/token";
 
-export async function getApplication(id: string) {
-  const res = await api.get(`/api/applications/${id}`);
-  return res.data;
-}
-
-export async function getApplicationFull(id: string) {
-  const res = await api.get(`/api/applications/${id}/full`);
-  return res.data;
-}
-
-export async function getDocuments(id: string) {
-  const res = await api.get(`/api/applications/${id}/documents`);
-  return res.data;
-}
-
-export async function getBanking(id: string) {
-  const res = await api.get(`/api/applications/${id}/banking`);
-  return res.data;
-}
-
-export async function getFinancials(id: string) {
-  const res = await api.get(`/api/applications/${id}/financials`);
-  return res.data;
-}
-
-export async function getOCR(id: string) {
-  const res = await api.get(`/api/applications/${id}/ocr`);
-  return res.data;
-}
-
-export async function getLenders(id: string) {
-  const res = await api.get(`/api/applications/${id}/lenders`);
-  return res.data;
+export async function fetchApplications() {
+  return httpGet(api.url("/api/applications"), getToken());
 }
