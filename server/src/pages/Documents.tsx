177659f8b6242a29949b5790c6008d43163b7a4a
werb210 <todd.w@boreal.financial>
2025-12-04 10:54:26 -0700

    Update Documents.tsx

--- server/src/pages/Documents.tsx
+++ server/src/pages/Documents.tsx
@@ -1,11 +1,8 @@
-import MainLayout from "../layouts/MainLayout";
-
 export default function Documents() {
   return (
-    <MainLayout>
-      <div style={{ padding: 20 }}>
-        <h1>Documents</h1>
-      </div>
-    </MainLayout>
+    <div>
+      <h1>Documents</h1>
+      <p>Document manager UI here.</p>
+    </div>
   );
 }
