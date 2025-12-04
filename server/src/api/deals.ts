1f02a2aa1a6f72dab84ebcc5ee6e2898d0d08d92
werb210 <todd.w@boreal.financial>
2025-12-04 12:22:51 -0700

    Update deals.ts

--- server/src/api/deals.ts
+++ server/src/api/deals.ts
@@ -1,24 +1,5 @@
-import { api } from "@/lib/http";
+import { http } from "@/lib/api/http";
 
-export interface Deal {
-  id: string;
-  companyId: string;
-  amount: number;
-  status: string;
-  createdAt: string;
-}
-
-export async function listDeals() {
-  const res = await api.get<Deal[]>("/deals");
-  return res.data;
-}
-
-export async function getDeal(id: string) {
-  const res = await api.get<Deal>(`/deals/${id}`);
-  return res.data;
-}
-
-export async function updateDeal(id: string, payload: Partial<Deal>) {
-  const res = await api.put<Deal>(`/deals/${id}`, payload);
-  return res.data;
-}
+export const DealsAPI = {
+  list: () => http.get("/deals"),
+};
