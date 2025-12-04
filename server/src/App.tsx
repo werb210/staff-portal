bccce305ba3026edd9430c08397590b117a0917b
werb210 <todd.w@boreal.financial>
2025-12-04 14:24:12 -0700

    Update App.tsx

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,15 +1,7 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from "./pages/auth/LoginPage";
-import DashboardPage from "./pages/dashboard/DashboardPage";
-
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="*" element={<div>Not Found</div>} />
-    </Routes>
-  );
+    <div style={{ padding: 40 }}>
+      <h1>Staff Portal Running</h1>
+    </div>
+  )
 }
