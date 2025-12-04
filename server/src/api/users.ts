e5cb17f7fba4f70042280c5c7770b38b11c5a5ac
werb210 <todd.w@boreal.financial>
2025-12-04 12:21:53 -0700

    Update users.ts

--- server/src/api/users.ts
+++ server/src/api/users.ts
@@ -1,18 +1,5 @@
-import { api } from "@/lib/http";
+import { http } from "@/lib/api/http";
 
-export interface User {
-  id: string;
-  email: string;
-  role: string;
-  name: string;
-}
-
-export async function listUsers() {
-  const res = await api.get<User[]>("/users");
-  return res.data;
-}
-
-export async function updateUserRole(id: string, role: string) {
-  const res = await api.post(`/users/${id}/role`, { role });
-  return res.data;
-}
+export const UsersAPI = {
+  list: () => http.get("/users"),
+};
