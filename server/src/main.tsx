4579228aefa4c3bc80a18211cdac9d64b9f709e2
werb210 <todd.w@boreal.financial>
2025-12-04 14:18:22 -0700

    Update main.tsx

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -4,12 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const rootEl = document.getElementById("root");
-if (!rootEl) {
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
   throw new Error("Root element #root not found in index.html");
 }
 
-ReactDOM.createRoot(rootEl).render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
