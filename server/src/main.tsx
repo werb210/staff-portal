39a77c4ec67d8a03a92902ed4dee6539cc403056
werb210 <todd.w@boreal.financial>
2025-12-04 13:44:28 -0700

    Update main.tsx

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { appRouter } from "./router/appRouter";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 const rootEl = document.getElementById("root");
-if (!rootEl) throw new Error("Root element #root not found");
 
-createRoot(rootEl).render(
+if (!rootEl) {
+  throw new Error("Root element #root not found in index.html");
+}
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <RouterProvider router={appRouter} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
