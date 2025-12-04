715d0a99869b30fe7089efe0619594c4eec364fe
werb210 <todd.w@boreal.financial>
2025-12-04 12:08:47 -0700

    Update MainLayout.tsx

--- server/src/layout/MainLayout.tsx
+++ server/src/layout/MainLayout.tsx
@@ -1,15 +1,17 @@
-import Sidebar from "./Sidebar";
-import TopBar from "./TopBar";
 import { Outlet } from "react-router-dom";
+import Sidebar from "@/nav/Sidebar";
+import NotificationBell from "@/nav/NotificationBell";
 
 export default function MainLayout() {
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
+    <div className="flex h-screen w-full">
       <Sidebar />
 
-      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-        <TopBar />
-        <div style={{ padding: "20px", overflowY: "auto", flex: 1 }}>
+      <div className="flex-1 flex flex-col">
+        <div className="p-4 border-b">
+          <NotificationBell />
+        </div>
+        <div className="p-4 flex-1 overflow-auto bg-white">
           <Outlet />
         </div>
       </div>
