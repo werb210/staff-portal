a806ac165e4778087f5b01961499516a35bde187
werb210 <todd.w@boreal.financial>
2025-12-04 13:16:24 -0700

    Update apiClient.ts

--- server/src/lib/apiClient.ts
+++ server/src/lib/apiClient.ts
@@ -1,15 +1,4 @@
-import axios from "axios";
-import { useAuthStore } from "@/state/authStore";
+// server/src/lib/apiClient.ts
+import { http } from "@/lib/http";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-});
-
-api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-export default api;
+export const apiClient = http;
