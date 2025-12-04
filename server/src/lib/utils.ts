b4426e568c3c4efae9fd738c322175ec4e3a650c
werb210 <todd.w@boreal.financial>
2025-12-04 10:47:28 -0700

    Update utils.ts

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,6 +1,2 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export const cls = (...c: (string | false | undefined | null)[]) =>
+  c.filter(Boolean).join(" ");
