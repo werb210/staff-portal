013b6cca57a5efb4ded0ba649c88b3a10b73935d
werb210 <todd.w@boreal.financial>
2025-12-04 10:55:35 -0700

    Update AdminUsersPage.tsx

--- server/src/pages/Admin/AdminUsersPage.tsx
+++ server/src/pages/Admin/AdminUsersPage.tsx
@@ -1,19 +1,8 @@
-import UsersTable from "../../components/users/UsersTable";
-import UserEditorModal from "../../components/users/UserEditorModal";
-import { useUsersStore } from "../../state/usersStore";
-
 export default function AdminUsersPage() {
-  const { openEditor } = useUsersStore();
-
   return (
-    <div style={{ padding: 24 }}>
-      <h1>Users</h1>
-      <button style={{ marginBottom: 16 }} onClick={() => openEditor(null)}>
-        + Create User
-      </button>
-
-      <UsersTable />
-      <UserEditorModal />
+    <div>
+      <h1>Admin – Users</h1>
+      <p>User management tools here.</p>
     </div>
   );
 }
