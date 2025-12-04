9834c5c2fb23766f99eab6c19fe2fa75e727a7e4
werb210 <todd.w@boreal.financial>
2025-12-04 13:14:36 -0700

    Update main.tsx

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -1,15 +1,17 @@
+// server/src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { appRouter } from "./router/appRouter";
-import { queryClient } from "./core/queryClient";
-import "./index.css";
+import App from "./App";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element #root not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={appRouter} />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
