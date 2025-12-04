db0376b83883751e0c0b90ed8b7baf5e1e05c641
werb210 <todd.w@boreal.financial>
2025-12-04 11:38:06 -0700

    Update MainLayout.tsx

--- server/src/layout/MainLayout.tsx
+++ server/src/layout/MainLayout.tsx
@@ -1,13 +1,18 @@
-import NavBar from "@/components/NavBar";
+import Sidebar from "./Sidebar";
+import TopBar from "./TopBar";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div className="app-shell">
-      <NavBar />
-      <main className="app-content">
-        <Outlet />
-      </main>
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar />
+
+      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <TopBar />
+        <div style={{ padding: "20px", overflowY: "auto", flex: 1 }}>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
