54de10c61584bc48fe7b2e0e24213afc7a25b806
werb210 <todd.w@boreal.financial>
2025-12-04 13:14:18 -0700

    Update App.tsx

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,6 +1,16 @@
+// server/src/App.tsx
 import { RouterProvider } from "react-router-dom";
-import { appRouter } from "./router/appRouter";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { appRouter } from "@/router/appRouter";
+import { queryClient } from "@/core/queryClient";
+import { useAuthInit } from "@/hooks/useAuthInit";
 
 export default function App() {
-  return <RouterProvider router={appRouter} />;
+  useAuthInit();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />
+    </QueryClientProvider>
+  );
 }
