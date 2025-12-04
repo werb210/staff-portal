c83434af01e5fd16c5ddb3fedf852c522d3c850d
werb210 <todd.w@boreal.financial>
2025-12-04 10:47:10 -0700

    Update http.ts

--- server/src/lib/http.ts
+++ server/src/lib/http.ts
@@ -1,26 +1,4 @@
-import axios from "axios";
-import { useAuthStore } from "@/state/authStore";
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  withCredentials: true
-});
-
-api.interceptors.request.use(config => {
-  const token = useAuthStore.getState().token;
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-api.interceptors.response.use(
-  res => res,
-  err => {
-    if (err.response?.status === 401) {
-      useAuthStore.getState().logout();
-      window.location.href = "/login";
-    }
-    return Promise.reject(err);
-  }
-);
-
-export default api;
+export async function http<T>(fn: () => Promise<{ data: T }>): Promise<T> {
+  const res = await fn();
+  return res.data;
+}
