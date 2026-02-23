a8115ad63250b18e697357eaf8d7ef38b4b6a6e5
werb210 <todd.w@boreal.financial>
2026-02-23 15:14:33 -0700

    Add BI commissions dashboard component and silo entry

---
+++ src/silos/bi/index.tsx
@@ -0,0 +1,10 @@
+import BiCommissions from "./BiCommissions";
+
+export default function BiSilo() {
+  return (
+    <div>
+      <h1>Boreal Insurance</h1>
+      <BiCommissions />
+    </div>
+  );
+}
